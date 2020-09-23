b0ad28c09d095402ad05b22ed8c760ed98b955d7
jpham35 <49501108+jpham35@users.noreply.github.com>
2020-09-23 10:00:50 -0500

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Jennifer Pham on 9/23/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
