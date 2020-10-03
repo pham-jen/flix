9741c330a2ff4ddf947fd6a6bd663d53fb1b7a3d
jpham35 <49501108+jpham35@users.noreply.github.com>
2020-10-03 00:49:54 -0500

    Completed Superhero Collection View page, added flow layout, added posterView connection

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Jennifer Pham on 10/2/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+
+    @IBOutlet weak var posterView: UIImageView!
+    
+    
+}
